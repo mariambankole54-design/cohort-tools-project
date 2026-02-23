3e601df461d7f4c326f78d127fb18b106d199ab9
Mariam Bankole <mariambankole54@gmail.com>
2026-02-23 16:47:33 +0000

    project

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -15,6 +16,11 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://example.com']
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -27,6 +33,12 @@ app.use(cookieParser());
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
+});
+app.get("/api/cohorts", (req, res) => {
+  res.sendFile(__dirname + "/cohorts.json");
+});
+app.get("/api/students", (req, res) => {
+  res.sendFile(__dirname + "/students.json");
 });
 
 
