52a28b8f9f33d0ddae89e5cf9e76e0501773337d
eren-brcn <sylar144814@gmail.com>
2026-02-24 16:47:37 +0300

    s

--- server/app.js
+++ server/app.js
@@ -2,10 +2,15 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-
+const cors = require("cors");
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
+ 
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -14,7 +19,11 @@ const app = express();
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
-// ...
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://example.com']
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -26,7 +35,7 @@ app.use(cookieParser());
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
+  res.sendFile(__dirname + "/views/cohorts.json");
 });
 
 
